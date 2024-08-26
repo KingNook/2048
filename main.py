15936adc62241585d662e83844efc741f1384d2c
Michael <mike04.lin@gmail.com>
2024-08-26 15:52:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ from pygame.locals import (
     QUIT
 )
 
-import colors
+import asyncio
 
+import colors
 import grid_objects
 
 pygame.init()
@@ -71,142 +72,150 @@ grid.reset_grid()
 ## ==========
 
 alive = True
-while alive:
+async def main():
 
-    ## ============
-    ## KEY HANDLING
-    ## ============
+    while alive:
 
-    for event in pygame.event.get():
+        ## ============
+        ## KEY HANDLING
+        ## ============
 
-        if event.type == KEYDOWN:
+        for event in pygame.event.get():
 
-            if event.key == K_ESCAPE:
+            if event.type == KEYDOWN:
+
+                if event.key == K_ESCAPE:
+                    alive = False
+
+                elif event.key == K_UP or event.key == K_w:
+                    grid.move_up()
+                elif event.key == K_LEFT or event.key == K_a:
+                    grid.move_left()
+                elif event.key == K_DOWN or event.key == K_s:
+                    grid.move_down()
+                elif event.key == K_RIGHT or event.key == K_d:
+                    grid.move_right()
+
+                elif event.key == K_r and grid.alive == False:
+                    grid.reset_grid()
+
+                # for debugging purposes
+                elif event.key == K_k:
+                    grid.alive = not grid.alive
+
+            elif event.type == QUIT:
                 alive = False
+                return
 
-            elif event.key == K_UP or event.key == K_w:
-                grid.move_up()
-            elif event.key == K_LEFT or event.key == K_a:
-                grid.move_left()
-            elif event.key == K_DOWN or event.key == K_s:
-                grid.move_down()
-            elif event.key == K_RIGHT or event.key == K_d:
-                grid.move_right()
+        ## ======
+        ## RENDER
+        ## ======
 
-            elif event.key == K_r and grid.alive == False:
-                grid.reset_grid()
+        # ---------------
+        # DRAW BACKGROUND
+        # ---------------
 
-            # for debugging purposes
-            elif event.key == K_k:
-                grid.alive = not grid.alive
-
-        elif event.type == QUIT:
-            alive = False
-            pygame.quit()
-            quit()
-
-    ## ======
-    ## RENDER
-    ## ======
-
-    # ---------------
-    # DRAW BACKGROUND
-    # ---------------
-
-    screen.fill(colors.IVORY)
-    
-    # grid background
-    pygame.draw.rect(
-        screen, colors.TGRAY,
-        pygame.Rect(GRID_LEFT, GRID_TOP, GRID_SIZE, GRID_SIZE),
-        width = 0, border_radius = BORDER_RADIUS
-    )
-
-    # empty tiles
-    for pg_coord in coord_to_pg.values():
+        screen.fill(colors.IVORY)
+        
+        # grid background
         pygame.draw.rect(
-            screen, colors.LGRAY,
-            pygame.Rect(pg_coord[0], pg_coord[1], TILE_SIZE, TILE_SIZE),
+            screen, colors.TGRAY,
+            pygame.Rect(GRID_LEFT, GRID_TOP, GRID_SIZE, GRID_SIZE),
             width = 0, border_radius = BORDER_RADIUS
         )
 
-    # -----
-    # SCORE
-    # -----
+        # empty tiles
+        for pg_coord in coord_to_pg.values():
+            pygame.draw.rect(
+                screen, colors.LGRAY,
+                pygame.Rect(pg_coord[0], pg_coord[1], TILE_SIZE, TILE_SIZE),
+                width = 0, border_radius = BORDER_RADIUS
+            )
 
-    score_text = TEST_FONT.render(
-        str(grid.score), True, colors.AFW
-    )
+        # -----
+        # SCORE
+        # -----
 
-    screen.blit(score_text, (10, 10))
-
-    # ----------
-    # DRAW TILES
-    # ----------
-    
-    for tile_coord, tile_value in grid.tiles.items():
-
-        try:
-            tile_style = colors.TILE_STYLES[tile_value]
-        except KeyError:
-            tile_style = colors.DEFAULT_STYLE
-
-        pg_coord = coord_to_pygame(tile_coord)
-
-        # draw tile
-        tile = pygame.draw.rect(
-            screen, tile_style[0],
-            pygame.Rect(pg_coord[0], pg_coord[1], TILE_SIZE, TILE_SIZE),
-            width = 0, border_radius = BORDER_RADIUS
+        score_text = TEST_FONT.render(
+            str(grid.score), True, colors.AFW
         )
 
-        # draw tile text
-        centre_coord = (
-            pg_coord[0] + TILE_SIZE//2,
-            pg_coord[1] + TILE_SIZE//2
-        )
+        screen.blit(score_text, (10, 10))
 
-        tile_text = TEST_FONT.render(
-            str(2**tile_value), True, tile_style[1]
-        )
+        # ----------
+        # DRAW TILES
+        # ----------
+        
+        for tile_coord, tile_value in grid.tiles.items():
 
-        text_rect = tile_text.get_rect(center = centre_coord)
+            try:
+                tile_style = colors.TILE_STYLES[tile_value]
+            except KeyError:
+                tile_style = colors.DEFAULT_STYLE
 
-        screen.blit(tile_text, text_rect)
+            pg_coord = coord_to_pygame(tile_coord)
 
-    # ---------
-    # GAME OVER
-    # ---------
+            # draw tile
+            tile = pygame.draw.rect(
+                screen, tile_style[0],
+                pygame.Rect(pg_coord[0], pg_coord[1], TILE_SIZE, TILE_SIZE),
+                width = 0, border_radius = BORDER_RADIUS
+            )
 
-    if grid.alive == False:
+            # draw tile text
+            centre_coord = (
+                pg_coord[0] + TILE_SIZE//2,
+                pg_coord[1] + TILE_SIZE//2
+            )
 
-        game_over_surface = pygame.Surface(
-            (GRID_SIZE, GRID_SIZE), pygame.SRCALPHA
-        )
+            tile_text = TEST_FONT.render(
+                str(2**tile_value), True, tile_style[1]
+            )
 
-        # overlay
-        pygame.draw.rect(
-            game_over_surface, colors.GAME_OVER,
-            game_over_surface.get_rect(), border_radius=BORDER_RADIUS
-        )
+            text_rect = tile_text.get_rect(center = centre_coord)
 
-        # game over text
-        centre_coord = (
-            GRID_SIZE // 2,
-            GRID_SIZE // 2
-        )
+            screen.blit(tile_text, text_rect)
 
-        game_over_text = TEST_FONT.render(
-            'Game Over', True, colors.DTEXT
-        )
+        # ---------
+        # GAME OVER
+        # ---------
 
-        text_rect = game_over_text.get_rect(center = centre_coord)
+        if grid.alive == False:
 
-        game_over_surface.blit(game_over_text, text_rect)
+            game_over_surface = pygame.Surface(
+                (GRID_SIZE, GRID_SIZE), pygame.SRCALPHA
+            )
 
-        screen.blit(game_over_surface, (GRID_LEFT, GRID_TOP))
+            # overlay
+            pygame.draw.rect(
+                game_over_surface, colors.GAME_OVER,
+                game_over_surface.get_rect(), border_radius=BORDER_RADIUS
+            )
+
+            # game over text
+            centre_coord = (
+                GRID_SIZE // 2,
+                GRID_SIZE // 2
+            )
+
+            game_over_text = TEST_FONT.render(
+                'Game Over', True, colors.DTEXT
+            )
+
+            text_rect = game_over_text.get_rect(center = centre_coord)
+
+            game_over_surface.blit(game_over_text, text_rect)
+
+            screen.blit(game_over_surface, (GRID_LEFT, GRID_TOP))
 
 
-    # ===================
-    pygame.display.flip()
-    # ===================
+        # ===================
+        pygame.display.flip()
+        # ===================
+
+        await asyncio.sleep(0)
+
+        if not alive:
+            return
+        
+asyncio.run(main())
