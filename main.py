6318a1416c327d37e6e278cd5885bd54cf9c7ceb
Michael <mike04.lin@gmail.com>
2024-09-14 18:42:13 +0100

    refactor: tile drawing

--- main.py
+++ main.py
@@ -152,6 +152,32 @@ def draw_score_text():
 
     return True
 
+def draw_tiles():
+    for tile_coord, tile_value in grid.tiles.items():
+
+        try:
+            tile_style = colors.TILE_STYLES[tile_value]
+        except KeyError:
+            tile_style = colors.DEFAULT_STYLE
+
+        pg_coord = coord_to_pygame(tile_coord)
+
+        # draw tile
+        tile = pygame.draw.rect(
+            screen, tile_style[0],
+            pygame.Rect(pg_coord[0], pg_coord[1], TILE_SIZE, TILE_SIZE),
+            width = 0, border_radius = BORDER_RADIUS
+        )
+
+        tile_text = TEST_FONT.render(
+            str(2**tile_value), True, tile_style[1]
+        )
+
+        text_rect = tile_text.get_rect(center = get_centre_coords(pg_coord))
+
+        screen.blit(tile_text, text_rect)
+
+
 ## ==========
 ## GAME CYCLE
 ## ==========
@@ -189,29 +215,7 @@ async def main():
         # DRAW TILES
         # ----------
         
-        for tile_coord, tile_value in grid.tiles.items():
-
-            try:
-                tile_style = colors.TILE_STYLES[tile_value]
-            except KeyError:
-                tile_style = colors.DEFAULT_STYLE
-
-            pg_coord = coord_to_pygame(tile_coord)
-
-            # draw tile
-            tile = pygame.draw.rect(
-                screen, tile_style[0],
-                pygame.Rect(pg_coord[0], pg_coord[1], TILE_SIZE, TILE_SIZE),
-                width = 0, border_radius = BORDER_RADIUS
-            )
-
-            tile_text = TEST_FONT.render(
-                str(2**tile_value), True, tile_style[1]
-            )
-
-            text_rect = tile_text.get_rect(center = get_centre_coords(pg_coord))
-
-            screen.blit(tile_text, text_rect)
+        draw_tiles()
 
         # ---------
         # GAME OVER
